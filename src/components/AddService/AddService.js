c68c0b28e94e3d2489e3a4d3b3b69a0aa04a6f20
Amirul-Hosain <amirulhosain77@gmail.com>
2021-10-29 09:47:03 +0600

    initial checkup

---
+++ src/components/AddService/AddService.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddService = () => {
+    return (
+        <div>
+            this is new service  adding.
+        </div>
+    );
+};
+
+export default AddService;
---
+++ src/components/Login/Logn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logn = () => {
+    return (
+        <div>
+            this is login section.
+        </div>
+    );
+};
+
+export default Logn;
